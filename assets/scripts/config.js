9377ee08a06cf8fd308f6f581dd1461a62506b8d
rainswerld <m.rainrobinson@gmail.com>
2020-08-07 14:03:32 -0600

    Heroku db in production

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://glacial-savannah-32363.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
