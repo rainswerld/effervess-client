e3b8ed7f110ccd2b68d71429033e2ee6101c14e9
rainswerld <m.rainrobinson@gmail.com>
2020-08-07 14:04:44 -0600

    Removed backslash from path to heroku on config

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://glacial-savannah-32363.herokuapp.com/',
+  production: 'https://glacial-savannah-32363.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
